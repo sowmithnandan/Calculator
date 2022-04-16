4df383b77c941fc056dcb8fcac87466b581c848b
sowmithnandan <sowmithnandanr@gmail.com>
2022-04-16 20:33:59 +0530

    3

--- calcTest.py
+++ calcTest.py
@@ -5,27 +5,29 @@ class CalcTest(unittest.TestCase):
     """
     Test the add function from the calc library
     """
-    def TestSqrt(self):
+    
+    def testSqrt(self):
         Choice = 2 
-        self.assertEqual(Calculator([4],Choice),2.0,"Answer is 2.0")
-        self.assertEqual(Calculator([16],Choice),4.0,"Answer is 4.0")
-        self.assertEqual(Calculator([100],Choice),10.0,"Answer is 10.0")
+        result = Calculator([4],Choice)
+        self.assertEqual(result,2.0)
+        self.assertNotEqual(Calculator([16],Choice),2.0,"Answer is 4.0")
+        self.assertNotEqual(Calculator([100],Choice),9.0,"Answer is 10.0")
         self.assertEqual(Calculator([5],Choice),2.23606797749979,"Answer is 2.0")
-    def TestFactorial(self):
+    def testFactorial(self):
         Choice = 3 
         self.assertEqual(Calculator([1],Choice),1,"Answer is 1.0")
         self.assertEqual(Calculator([2],Choice),2,"Answer is 2.0")
-        self.assertEqual(Calculator([4],Choice),24,"Answer is 24.0")
+        self.assertNotEqual(Calculator([4],Choice),22,"Answer is 24.0")
         self.assertEqual(Calculator([5],Choice),120,"Answer is 120.0")
-    def TestNatLog(self):
+    def testNatLog(self):
         Choice = 4
-        self.assertEqual(Calculator([1],Choice),0,"Answer is 0")
+        self.assertNotEqual(Calculator([1],Choice),1,"Answer is 0")
         self.assertEqual(Calculator([2],Choice),0.6931471805599453,"Answer is 0.6931471805599453")
         self.assertEqual(Calculator([10],Choice),2.302585092994046,"Answer is 2.302585092994046")
-    def TestPow(self):
+    def testPow(self):
         Choice = 5      
         self.assertEqual(Calculator([100,0],Choice),1.0,"Answer is 1.0")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-        self.assertEqual(Calculator([2,2],Choice),4.0,"Answer is 4.0")
+        self.assertNotEqual(Calculator([2,2],Choice),5.0,"Answer is 4.0")
         self.assertEqual(Calculator([10,3],Choice),1000.0,"Answer is 1000.0")
         self.assertEqual(Calculator([5,4],Choice),625,"Answer is 625.0")
 
@@ -70,3 +72,8 @@ class CalcTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    # CalcTestObject = CalcTest()
+    # CalcTestObject.TestFactorial()
+    # CalcTestObject.TestNatLog()
+    # CalcTestObject.TestPow()
+    # CalcTestObject.TestSqrt()
--- calculator.py
+++ calculator.py
@@ -11,42 +11,48 @@ def Calculator(Input,Choice):
     if Choice==2:
         return  math.sqrt(Input[0])
     elif Choice==3:
+        # Ans = Input[0]
+        # for i in /range(2,Input[0]+1):
+            # Ans = Ans*i 
         return FactorialFunc(Input[0])
     elif Choice==4:
         return math.log(Input[0])
     elif Choice==5:
         return pow(Input[0],Input[1])
     return 0  
-
-while True:
-    print("Enter you option:your options are")
-    print("1: Exit") 
-    print("2: Square Root Function")
-    print("3: Factorial Function") 
-    print("4: Natural Logarithm ")
-    print("5: Power Function")
-    Choice = int(input())
-    if Choice ==1:
-        break
-    elif Choice==2: 
-        print("Enter your number:")
-        Number1 = int(input())
-        print("Result is:"+str(Calculator([Number1],Choice)))
-    elif Choice==3:
-        print("Enter your number:")
-        Number1 = int(input())
-        print("Result is:"+str(Calculator([Number1],Choice)))
-    elif Choice==4: 
-        print("Enter your number:")
-        Number1 = int(input())
-        print("Result is:"+str(Calculator([Number1],Choice))  )
-    else: 
-        print("Enter your number:")
-        Number1 = int(input())
-        print("Enter your number:")
-        Number2 = int(input())
-        print("Result is:"+str(Calculator([Number1,Number2],Choice)))   
-print("Calculator Program has been forfeited");    
+if __name__ == '__main__':
+    while True:
+        print("Enter you option:your options are")
+        print("1: Exit") 
+        print("2: Square Root Function")
+        print("3: Factorial Function") 
+        print("4: Natural Logarithm ")
+        print("5: Power Function")
+        Choice = int(input())
+        if Choice ==1:
+            break
+        elif Choice==2: 
+            print("Enter your number:")
+            Number1 = int(input())
+            print("Result is:"+str(Calculator([Number1],Choice)))
+        elif Choice==3:
+            print("Enter your number:")
+            Number1 = int(input())
+            print("Result is:"+str(Calculator([Number1],Choice)))
+        elif Choice==4: 
+            print("Enter your number:")
+            Number1 = int(input())
+            print("Result is:"+str(Calculator([Number1],Choice))  )
+        elif Choice==5: 
+            print("Enter your number:")
+            Number1 = int(input())
+            print("Enter your number:")
+            Number2 = int(input())
+            print("Result is:"+str(Calculator([Number1,Number2],Choice)))   
+        else:
+            print("Option not recognized")
+            break
+    print("Calculator Program has been forfeited");    
 
 
 
