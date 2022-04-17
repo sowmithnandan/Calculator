cf8dcb46010e25e15e2014ff981ace38d28e5cac
sowmithnandan <sowmithnandanr@gmail.com>
2022-04-17 18:27:21 +0530

    Added error handling and logging for errors in the base code

--- calculator.py
+++ calculator.py
@@ -15,17 +15,30 @@ def FactorialFunc(n):
 
 def Calculator(Input,Choice):
     if Choice==2:
-        return  math.sqrt(Input[0])
+        if (Input[0]>=0):
+            return  math.sqrt(Input[0])
+        else:
+            logging.error("[SQRT] input less than zero")
+            return -1
     elif Choice==3:
-        # Ans = Input[0]
-        # for i in /range(2,Input[0]+1):
-            # Ans = Ans*i 
-        return FactorialFunc(Input[0])
+        if (Input[0]>=0):
+            return FactorialFunc(Input[0])
+        else:
+            logging.error("[FACTORIAL] input less than zero")
+            return -1
     elif Choice==4:
-        return math.log(Input[0])
+        if (Input[0]>0):
+            return math.log(Input[0])
+        else:
+            logging.error("[NATLOG] input less than or equal to zero")
+            return -1
     elif Choice==5:
+        if Input[0]==0:
+            logging.error("[POW] base is 0")
+            return "Fail"
         return pow(Input[0],Input[1])
     return 0  
+
 if __name__ == '__main__':
     while True:
         logging.info("[MASTER] Options are printed on the users screen")
@@ -35,36 +48,59 @@ if __name__ == '__main__':
         print("3: Factorial Function") 
         print("4: Natural Logarithm ")
         print("5: Power Function")
-        Choice = int(input())
-        logging.info("[]Master] Init Calculator")
-        if Choice ==1:
-            break
-        elif Choice==2: 
-            print("Enter your number:")
-            Number1 = int(input())
-            logging.info("[SQRT] Getting Square Root of " + str(Number1))
-            print("Result is:"+str(Calculator([Number1],Choice)))
-        elif Choice==3:
-            print("Enter your number:")
-            Number1 = int(input())
-            logging.info("[FACTORIAL] Getting the factorial of" + str(Number1))
-            print("Result is:"+str(Calculator([Number1],Choice)))
-        elif Choice==4: 
-            print("Enter your number:")
-            Number1 = int(input())
-            logging.info("[NATLOG] Getting the logarithm of" + str(Number1))
-            print("Result is:"+str(Calculator([Number1],Choice))  )
-        elif Choice==5: 
-            print("Enter your number:")
-            Number1 = int(input())
-            print("Enter your number:")
-            Number2 = int(input())
-            logging.info("[POW] Returning " + str(Number1) + " Raised to the power of "+ str(Number2))
-            print("Result is:"+str(Calculator([Number1,Number2],Choice)))   
-        else:
-            logging.info("[MASTER] Unknown user input detected")
-            print("Option not recognized")
-            break
+        try:     
+            Choice = int(input())
+            logging.info("[Master] Init Calculator")
+            if Choice ==1:
+                break
+            elif Choice==2: 
+                print("Enter your number:")
+                Number1 = int(input())
+                logging.info("[SQRT] Getting Square Root of " + str(Number1))
+                result = Calculator([Number1],Choice)
+                if result==-1:
+                    print("Incorrect input for the selected option")
+                    logging.info("[MASTER] Restarting Calculator ")
+                    continue 
+                print("Result is:"+str(result))
+            elif Choice==3:
+                print("Enter your number:")
+                Number1 = int(input())
+                logging.info("[FACTORIAL] Getting the factorial of" + str(Number1))
+                result = Calculator([Number1],Choice)
+                if result==-1:
+                    print("Incorrect input for the selected option")
+                    logging.info("[MASTER] Restarting Calculator ")
+                    continue 
+                print("Result is:"+str(result))
+            elif Choice==4: 
+                print("Enter your number:")
+                Number1 = int(input())
+                logging.info("[NATLOG] Getting the logarithm of" + str(Number1))
+                result = Calculator([Number1],Choice)
+                if result==-1:
+                    print("Incorrect input for the selected option")
+                    logging.info("[MASTER] Restarting Calculator ")
+                    continue 
+                print("Result is:"+str(result)  )
+            elif Choice==5: 
+                print("Enter your base:")
+                Number1 = int(input())
+                print("Enter your power:")
+                Number2 = int(input())
+                result = Calculator([Number1,Number2],Choice)
+                if result=="Fail":
+                    print("Incorrect input for the selected option")
+                    logging.info("[MASTER] Restarting Calculator ")
+                    continue 
+                logging.info("[POW] Returning " + str(Number1) + " Raised to the power of "+ str(Number2))
+                print("Result is:"+str(result ))   
+            else:
+                logging.error("[MASTER] Unknown user input detected, restarting calculator")
+                print("Option not recognized")
+        except:
+            logging.error("[MASTER]Incorrect User Input detected")
+            break 
     print("Calculator Program has been forfeited");    
 
 
