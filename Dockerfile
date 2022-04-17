163ae8ab04ae8d893ff700236480740890475dac
sowmithnandan <sowmithnandanr@gmail.com>
2022-04-17 18:50:07 +0530

    Clean up

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,3 @@
-# Created Dockerfile using multi-stage setup
-# FROM maven:3.6.3-jdk-11-slim AS build
-# WORKDIR /app
-# RUN mkdir src
-# COPY src ./src
-# COPY pom.xml .
-# # Build Code
-# RUN ls /app
-# RUN mvn clean package
-# # Test code
-# RUN mvn test
-# # ENTRYPOINT ["java","-cp","./target/devops_scientific_calc-0.0.1-SNAPSHOT.jar", "devops_scientific_calc.Application"]
-
-# # Run Code
-# FROM openjdk:11-jre-slim
-# WORKDIR /home
-# RUN ls /home
-# COPY --from=build /app/target/devops_scientific_calc-0.0.1-SNAPSHOT.jar devops_scientific_calc-0.0.1-SNAPSHOT.jar
-# ENTRYPOINT ["java","-cp","/home/devops_scientific_calc-0.0.1-SNAPSHOT.jar", "devops_scientific_calc.Application"]
-
 FROM python:2-alpine AS Step 
 ENV PYTHONBUFFERED 1
 COPY . . 
--- calcTest.py
+++ calcTest.py
@@ -31,49 +31,5 @@ class CalcTest(unittest.TestCase):
         self.assertEqual(Calculator([10,3],Choice),1000.0,"Answer is 1000.0")
         self.assertEqual(Calculator([5,4],Choice),625,"Answer is 625.0")
 
-
-    # def test_add_integers(self):
-    #     """
-    #     Test that the addition of two integers returns the correct total
-    #     """
-    #     result = calc.add2(1, 2)
-    #     self.assertEqual(result, 3)
-
-    # def test_add_floats(self):
-    #     """
-    #     Test that the addition of two floats returns the correct result
-    #     """
-    #     result = calc.add2('10.5', 2)
-    #     self.assertEqual(result, 12.5)
-
-    # def test_add_strings(self):
-    #     """
-    #     Test the addition of two strings returns the two strings as one
-    #     concatenated string
-    #     """
-    #     result = calc.add2('abc', 'def')
-    #     self.assertEqual(result, 'abcdef')
-
-    # def test_add_string_and_integer(self):
-    #     """
-    #     Test the addition of a string and an integer returns them as one
-    #     concatenated string (in which the integer is converted to a string)
-    #     """
-    #     result = calc.add2('abc', 3)
-    #     self.assertEqual(result, 'abc3')
-
-    # def test_add_string_and_number(self):
-    #     """
-    #     Test the addition of a string and a float returns them as one
-    #     concatenated string (in which the float is converted to a string)
-    #     """
-    #     result = calc.add2('abc', '5.5')
-    #     self.assertEqual(result, 'abc5.5')
-
 if __name__ == '__main__':
     unittest.main()
-    # CalcTestObject = CalcTest()
-    # CalcTestObject.TestFactorial()
-    # CalcTestObject.TestNatLog()
-    # CalcTestObject.TestPow()
-    # CalcTestObject.TestSqrt()
